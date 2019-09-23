ff2a8f0a0d8873f71f33f74d79d1240f15222160
treemmett <tregan@tregan.me>
2019-09-23 17:25:41 -0600

    feat: add all stats

--- src/lambda/load.ts
+++ src/lambda/load.ts
@@ -13,23 +13,53 @@ export async function handler() {
     })
   ]);
 
-  const stats = gameData.playerstats.stats as { name: string; value: number }[];
+  const gameStats = gameData.playerstats.stats as {
+    name: string;
+    value: number;
+  }[];
 
-  const stat = stats.find(s => s.name === 'total_kills_awp');
+  function findStat(property: string): number {
+    const stat = gameStats.find(s => s.name === property);
 
-  let awpKills = 0;
-  if (stat) {
-    awpKills = stat.value;
+    if (!stat) {
+      return 0;
+    }
+
+    return stat.value;
   }
 
   const name = playerData.response.players[0].personaname;
   const pic = playerData.response.players[0].avatarfull;
+
+  const stats = {
+    kills: {
+      kills: findStat('total_kills'),
+      deaths: findStat('total_deaths'),
+      kd: (findStat('total_kills') / findStat('total_deaths')).toFixed(3)
+    },
+    objectives: {
+      bombPlants: findStat('total_planted_bombs'),
+      bombDefusal: findStat('total_defused_bombs')
+    },
+    matches: {
+      roundsWon: findStat('total_wins'),
+      wins: findStat('total_matches_won'),
+      total: findStat('total_matches_won')
+    },
+    weapons: {
+      ak47: findStat('total_kills_ak47'),
+      awp: findStat('total_kills_awp'),
+      deagle: findStat('total_kills_deagle'),
+      knife: findStat('total_kills_knife'),
+      m4a1: findStat('total_kills_m4a1')
+    }
+  };
 
   return {
     statusCode: 202,
     headers: {
       'content-type': 'application/json'
     },
-    body: JSON.stringify({ awpKills, name, pic })
+    body: JSON.stringify({ stats, name, pic })
   };
 }
--- src/web/views/Home.tsx
+++ src/web/views/Home.tsx
@@ -2,18 +2,42 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './Home.scss';
 
+interface Stats {
+  kills: {
+    kills: number;
+    deaths: number;
+    kd: number;
+  };
+  objectives: {
+    bombPlants: number;
+    bombDefusal: number;
+  };
+  matches: {
+    roundsWon: number;
+    wins: number;
+    total: number;
+  };
+  weapons: {
+    ak47: number;
+    awp: number;
+    deagle: number;
+    knife: number;
+    m4a1: number;
+  };
+}
+
 const Home: React.FC = () => {
   const [name, setName] = useState('');
   const [pic, setPic] = useState('');
-  const [awpKills, setAK] = useState(0);
+  const [stats, setStats] = useState({} as Stats);
 
   async function load() {
     const { data } = (await axios.get('/.netlify/functions/load')) as {
-      data: { awpKills: number; name: string; pic: string };
+      data: { stats: Stats; name: string; pic: string };
     };
     setName(data.name);
     setPic(data.pic);
-    setAK(data.awpKills);
+    setStats(data.stats);
   }
 
   useEffect(() => {
@@ -29,9 +53,80 @@ const Home: React.FC = () => {
 
       <div className={styles.stats}>
         <div className={styles.name}>Stats</div>
+
         <div className={styles.prop}>
           <div className={styles.property}>AWP Kills</div>
-          <div className={styles.value}>{awpKills}</div>
+          <div className={styles.value}>
+            {stats.weapons && stats.weapons.awp}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Knife Kills</div>
+          <div className={styles.value}>
+            {stats.weapons && stats.weapons.knife}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>AK47 Kills</div>
+          <div className={styles.value}>
+            {stats.weapons && stats.weapons.ak47}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>M4 Kills</div>
+          <div className={styles.value}>
+            {stats.weapons && stats.weapons.m4a1}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Deagle Kills</div>
+          <div className={styles.value}>
+            {stats.weapons && stats.weapons.deagle}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Total kills</div>
+          <div className={styles.value}>{stats.kills && stats.kills.kills}</div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Total deaths</div>
+          <div className={styles.value}>
+            {stats.kills && stats.kills.deaths}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>KD</div>
+          <div className={styles.value}>
+            {stats.kills && stats.kills.deaths}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Match wins</div>
+          <div className={styles.value}>
+            {stats.matches && stats.matches.wins}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Bombs planted</div>
+          <div className={styles.value}>
+            {stats.objectives && stats.objectives.bombPlants}
+          </div>
+        </div>
+
+        <div className={styles.prop}>
+          <div className={styles.property}>Bombs defused</div>
+          <div className={styles.value}>
+            {stats.objectives && stats.objectives.bombDefusal}
+          </div>
         </div>
       </div>
     </div>
